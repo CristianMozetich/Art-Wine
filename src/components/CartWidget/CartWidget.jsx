b11e466616aad5223678a53e2d74c86358e2d50a
Cristian Mozetich <cristianmozetich@gmail.com>
2023-04-29 15:30:14 +0100

    primer commit

---
+++ src/components/CartWidget/CartWidget.jsx
@@ -0,0 +1,14 @@
+
+import './CartWidget.css'
+
+const CartWidget = () => {
+    const cartImg = "../src/assets/img/cart.png"
+    return (
+        <div className='d-flex'>
+            <img className='cart m-2' src={cartImg} alt="cart" />
+            <p>10</p>
+        </div>
+  )
+}
+
+export default CartWidget
---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,39 @@
+
+import './NavBar.css'
+import CartWidget from '../CartWidget/CartWidget'
+
+const NavBar = () => {
+  const imgLogo = "../src/assets/img/argLogo.avif"
+  return (
+      <header className="navbar navbar-expand-lg bg-body-tertiary">
+        <div className="container-fluid">
+          <div className='d-flex'>
+            <h2>Mercado Argentino</h2>
+            <img className='imgLogo m-2' src={imgLogo} alt="argLogo"/>
+          </div>
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon" />
+          </button>
+          <nav className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav">
+              <li className="nav-item m-4">
+                Nosotros
+              </li>
+              <li className="nav-item m-4">
+                Productos Destacados
+              </li>
+              <li className="nav-item m-4">
+                Productos Tradicionales
+              </li>
+              <li className="nav-item m-4">
+                Contacto
+              </li>
+            </ul>
+          </nav>
+          <CartWidget />
+        </div>
+      </header>
+  )
+}
+
+export default NavBar
