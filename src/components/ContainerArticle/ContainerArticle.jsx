a5a2f3cf5e9d5772acc1b4c949c9b45a4ca59000
Cristian Mozetich <cristianmozetich@gmail.com>
2023-05-13 12:55:39 +0100

    modificaciones de estilos

--- src/components/ContainerArticle/ContainerArticle.jsx
+++ src/components/ContainerArticle/ContainerArticle.jsx
@@ -7,8 +7,6 @@ const ContainerArticle = () => {
     <Article title="ART & WINE" img="../../../assets/img/vinoArt.avif" >
         <img className='m-2' src="../../../assets/img/vinosArt.avif" alt="wine" />
         <img className='m-2' src="../../../assets/img/wineArt2.jpg" alt="vino" />
-    </Article>
-    <Article img="../../../assets/img/wineArt.avif">
         <strong>esto es un agregado de contenido a un articulo mediante "children"</strong>
     </Article>
     </>
