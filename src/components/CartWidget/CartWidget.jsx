1c6ef026cf31cd150dff6a9cc8d36c10f8a2326b
Cristian Mozetich <cristianmozetich@gmail.com>
2023-05-27 15:20:11 +0100

    correccion img

--- src/components/CartWidget/CartWidget.jsx
+++ src/components/CartWidget/CartWidget.jsx
@@ -2,7 +2,7 @@
 import './CartWidget.css'
 
 const CartWidget = () => {
-    const cartImg = "../../../public/assets/img/cartwhite-removebg-preview.png"
+    const cartImg = "../assets/img/cartwhite-removebg-preview.png"
     return (
         <div className='m-2 p-2 justify-content-center d-flex'>
             <img className='cart m-0 p-0' src={cartImg} alt="cart" />
