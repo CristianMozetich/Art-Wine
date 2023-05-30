e1954c53a35151494dac077b713eeb14fbfab230
Cristian Mozetich <cristianmozetich@gmail.com>
2023-05-30 20:59:25 +0100

    agrege firebase

--- src/components/CartWidget/CartWidget.jsx
+++ src/components/CartWidget/CartWidget.jsx
@@ -4,7 +4,7 @@ import './CartWidget.css'
 const CartWidget = () => {
     const cartImg = "../assets/img/cartwhite-removebg-preview.png"
     return (
-        <div className='m-2 p-2 justify-content-center d-flex'>
+        <div className='m-2 p-2 d-flex'>
             <img className='cart m-0 p-0' src={cartImg} alt="cart" />
             <p>10</p>
         </div>
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -17,7 +17,7 @@ const NavBar = () => {
           <button className="navbar-toggler m-3 bg-secondary"  type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon" />
           </button>
-          <nav className="collapse navbar-collapse justify-content-start" id="navbarNav">
+          <nav className="collapse navbar-collapse justify-content-center" id="navbarNav">
             <ul className="navbar-nav align-items-center">
               <li className="nav-item m-3">
                 <Link to={"/"} style={linkStyle}>Home</Link>
@@ -35,10 +35,10 @@ const NavBar = () => {
               <li className="nav-item m-3">
                 <NavLink to={`/formulario`} style={linkStyle}>Tastings & Events</NavLink>
               </li>
+              <CartWidget/>
             </ul>
           </nav>
         </div>
-        <CartWidget/>
       </header>
   )
 }
