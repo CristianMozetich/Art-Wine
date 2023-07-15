e5dd9a462221dcd1dfbe4c427f15682cc8400fec
Cristian Mozetich <cristianmozetich@gmail.com>
2023-07-15 21:21:41 +0100

    nav change

--- src/components/Article/Article.jsx
+++ src/components/Article/Article.jsx
@@ -10,7 +10,7 @@ const Article = ({title, img, children}) => {
   }, [])
 
   return (
-    <article className='artWine justify-content-center m-2 flex-wrap'>
+    <article className='artWine justify-content-center flex-wrap'>
         <h2 className='m-2' data-aos="fade-right" data-aos-duration="1000">{title}</h2>
         <h6 className='m-2 text-center' data-aos="fade-left" data-aos-duration="1000">Indulge in the mesmerizing fusion of art and wine at our store. Embark on a sensory journey through the captivating world of exceptional wines, where each sip unveils a symphony of flavors and delights.</h6>
         <img className='imgArticle m-2' src={img} alt={title} />
